79aaad23a61e724e3680b17eae421936025a94c6
James F <james@vancitystudios.com>
2020-11-01 19:58:49 -0800

    Update index.tsx

--- src/avatar/top/index.tsx
+++ src/avatar/top/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 
 import Eyepatch from './Eyepatch'
 import Hat from './Hat'
-import Helmet from './Helmet'
 import Hijab from './Hijab'
 import LongHairBigHair from './LongHairBigHair'
 import LongHairBob from './LongHairBob'
@@ -46,7 +45,6 @@ export default class Top extends React.Component {
         <NoHair>{children}</NoHair>
         <Eyepatch>{children}</Eyepatch>
         <Hat>{children}</Hat>
-        <Helmet>{children}</Helmet>
         <Hijab>{children}</Hijab>
         <Turban>{children}</Turban>
         <WinterHat1>{children}</WinterHat1>
