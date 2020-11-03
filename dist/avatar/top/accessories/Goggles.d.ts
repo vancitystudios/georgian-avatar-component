9409750f093e4b0fe07f4702a930df6297b9bd1f
James F <james@vancitystudios.com>
2020-11-03 11:35:51 -0800

    Goggles fix

---
+++ dist/avatar/top/accessories/Goggles.d.ts
@@ -0,0 +1,21 @@
+import * as React from 'react';
+export default class Goggles extends React.Component {
+    static optionValue: string;
+    private mask1;
+    private mask2;
+    private mask3;
+    private mask4;
+    private mask5;
+    private mask6;
+    private mask7;
+    private path1;
+    private path2;
+    private path3;
+    private path4;
+    private path5;
+    private path6;
+    private path7;
+    private linearGradient1;
+    private linearGradient2;
+    render(): JSX.Element;
+}
