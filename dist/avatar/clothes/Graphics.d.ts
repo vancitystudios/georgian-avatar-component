20c5efc381e5f3d6e4c45d1e3b411ba3b1108fe5
James F <james@vancitystudios.com>
2020-11-04 15:58:17 -0800

    Add Champion Graphic

--- dist/avatar/clothes/Graphics.d.ts
+++ dist/avatar/clothes/Graphics.d.ts
@@ -2,6 +2,10 @@ import * as React from 'react';
 export interface Props {
     maskID: string;
 }
+export declare class Champion extends React.Component<Props> {
+    static optionValue: string;
+    render(): JSX.Element;
+}
 export declare class CharlieChangemaker extends React.Component<Props> {
     static optionValue: string;
     render(): JSX.Element;
