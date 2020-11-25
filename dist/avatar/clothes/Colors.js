b18fd5c3d0bde8d8e46884f9b43fccced022eebb
James F <james@vancitystudios.com>
2020-11-25 12:31:52 -0800

    Update Shirt Colors

--- dist/avatar/clothes/Colors.js
+++ dist/avatar/clothes/Colors.js
@@ -32,21 +32,19 @@ function makeColor(name, color) {
     anyComponent.optionValue = name;
     return anyComponent;
 }
-var Black = makeColor('Black', '#262E33');
-var Blue01 = makeColor('Blue01', '#65C9FF');
-var Blue02 = makeColor('Blue02', '#5199E4');
-var Blue03 = makeColor('Blue03', '#25557C');
-var Gray01 = makeColor('Gray01', '#E6E6E6');
-var Gray02 = makeColor('Gray02', '#929598');
+var Black = makeColor('Changemaker Black', '#262E33');
+var Orange = makeColor('Changemaker Orange', '#e57200');
+var GBlue = makeColor('Georgian Blue', '#004b87');
+var GGreen = makeColor('Georgian Green', '#64a70b');
+var Blue01 = makeColor('Blue 01', '#65C9FF');
+var Blue02 = makeColor('Blue 02', '#5199E4');
+var Blue03 = makeColor('Blue 03', '#25557C');
+var Gray01 = makeColor('Gray 01', '#E6E6E6');
+var Gray02 = makeColor('Gray 02', '#929598');
 var Heather = makeColor('Heather', '#3C4F5C');
-var PastelBlue = makeColor('PastelBlue', '#B1E2FF');
-var PastelGreen = makeColor('PastelGreen', '#A7FFC4');
-var PastelOrange = makeColor('PastelOrange', '#FFDEB5');
-var PastelRed = makeColor('PastelRed', '#FFAFB9');
-var PastelYellow = makeColor('PastelYellow', '#FFFFB1');
+var PastelRed = makeColor('Pastel Red', '#FFAFB9');
 var Pink = makeColor('Pink', '#FF488E');
 var Red = makeColor('Red', '#FF5C5C');
-var White = makeColor('White', '#FFFFFF');
 var Colors = /** @class */ (function (_super) {
     __extends(Colors, _super);
     function Colors() {
@@ -55,20 +53,18 @@ var Colors = /** @class */ (function (_super) {
     Colors.prototype.render = function () {
         return (React.createElement(options_1.Selector, { option: options_1.ClotheColorOption, defaultOption: Gray01 },
             React.createElement(Black, { maskID: this.props.maskID }),
+            React.createElement(Orange, { maskID: this.props.maskID }),
+            React.createElement(GBlue, { maskID: this.props.maskID }),
+            React.createElement(GGreen, { maskID: this.props.maskID }),
             React.createElement(Blue01, { maskID: this.props.maskID }),
             React.createElement(Blue02, { maskID: this.props.maskID }),
             React.createElement(Blue03, { maskID: this.props.maskID }),
             React.createElement(Gray01, { maskID: this.props.maskID }),
             React.createElement(Gray02, { maskID: this.props.maskID }),
             React.createElement(Heather, { maskID: this.props.maskID }),
-            React.createElement(PastelBlue, { maskID: this.props.maskID }),
-            React.createElement(PastelGreen, { maskID: this.props.maskID }),
-            React.createElement(PastelOrange, { maskID: this.props.maskID }),
             React.createElement(PastelRed, { maskID: this.props.maskID }),
-            React.createElement(PastelYellow, { maskID: this.props.maskID }),
             React.createElement(Pink, { maskID: this.props.maskID }),
-            React.createElement(Red, { maskID: this.props.maskID }),
-            React.createElement(White, { maskID: this.props.maskID })));
+            React.createElement(Red, { maskID: this.props.maskID })));
     };
     return Colors;
 }(React.Component));
